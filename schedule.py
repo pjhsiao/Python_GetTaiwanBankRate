d13d5d55141fa74fbc7d098ece9eb4295ef348a5
pjhsiao <pjhsiao99@gmail.com>
2016-08-02 14:31:07 +0800

    modify schedule.py

--- schedule.py
+++ schedule.py
@@ -6,27 +6,22 @@ sched = BlockingScheduler()
 
 # @sched.scheduled_job('interval', seconds=10)
 # def timed_job():
-#     currentRate = parseRate.getCurrentRate()
-#     if parseRate.alarmRate > currentRate:
-#         gmailSender.send("The JPY rate that is over alarm now {}".format(currentRate))
-#         print("success for send mail")
-#     else:
-#         print("[{}] The current rate {} that is not over the alarm.".format(datetime.datetime.now(), currentRate))
+#     pass
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour='9-17', minute='0-59')
 def jpy_job():
     currentRate = parseRate.getCurrentRate()
+    if None == currentRate:
+        print("The rate value have not get")
+        return
+
     if parseRate.alarmRate > currentRate:
-        toAddrs = ['tomail@mail.com']
+        toAddrs = ['to@gmail.com']
         sender = gmailSender.GmailSender()
         sender.send(toAddrs, "The JPY rate that is over alarm now {}".format(currentRate))
-        print( "success for send mail")
+        print("success for send mail")
     else:
         print("[{}] The current rate {} that is not over the alarm.".format(datetime.datetime.now(), currentRate))
-
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour='9-17', minute='0-59')
-# def gbp_job():
-#     pass
 
 sched.start()
 
